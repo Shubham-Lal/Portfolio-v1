cf5d891798f940b3e4f872a3476f0a8b0e259309
Shubham Lal <im.shubhamlal@gmail.com>
2024-01-03 21:51:09 +0530

    update About me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -17,21 +17,14 @@ export default function About() {
                 <div className="about__data">
                     <Info />
 
-                    <p className="about__description">
-                        <span className="about__hobbies">Full-Stack Developer | Software Developer | Game Developer | App Developer</span>
-                        <br />
-                        <span>
-                            As a developer, my journey has been a remarkable one, encompassing a vast array of experiences. From the humble beginnings of saving PowerPoint slides as HTML webpages to crafting intricate Full-Stack Web applications, I have embarked on a journey of constant learning and growth. With a passion for gaming, I have ventured into the exciting world of Video Game design, where I continue to explore and innovate.
-                        </span>
-                        <br />
-                        {/* <span>
-                            Throughout my journey, I have come to appreciate the value of dedication and commitment. I firmly believe that with unwavering determination and an insatiable appetite for knowledge, we can learn something new every day. My parents have been a constant source of encouragement, providing the support and guidance that I needed to pursue my dreams. Alongside them, my brother-like friends have been an integral part of my journey, offering unwavering support and camaraderie.
-                        </span>
-                        <br />
-                        <span>
-                            With such incredible support, I am confident of a bright future filled with boundless opportunities. I look forward to continuing my journey of discovery, exploring new horizons and pushing the limits of what is possible.
-                        </span> */}
-                    </p>
+                    <div className="about__description">
+                        <p className="about__hobbies">Full-Stack Developer | Software Developer | Game Developer | App Developer<br /><br /></p>
+                        <p>
+                            With over 1200+ hours (ref. <a href="https://wakatime.com/@shubhamlal">wakatime.com/@shubhamlal</a>) of hands-on development experience. Actively engaged in building back-end solutions for various web applications.<br /><br />
+                            My greatest strength in development lies in breaking down product requirements into manageable stages, enabling me to address each component. This approach ensures that the applications I build not only excel in functionality but also possess the capability for troubleshooting and optimization in the near future.<br /><br />
+                            My proficiency extends beyond MERN stack, as I am currently learning and applying skills to build applications with Angular, PHP, and various other tech stacks.
+                        </p>
+                    </div>
 
                     <a href="/Resume-Shubham-Lal.pdf" className="button button--flex" target="_blank" rel="noopener noreferrer" >
                         My Resume
